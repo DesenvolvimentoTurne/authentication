e702f7866e0b4d9cb347c274d4562005a2ea81c8
TozziPaulo <33462630+TozziPaulo@users.noreply.github.com>
2018-09-02 12:19:29 -0300

    alteracao
    alteracao 1

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
-module.exports = {  
+module.exports = {
   url: 'mongodb://admin01:admin01@ds141942.mlab.com:41942/expressauth',
 };
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/login', function(req, res, next) {
 });
 
 router.get('/signup', function(req, res) {
-  res.render('signup.ejs', { message: req.flash('loginMessage') });
+  res.render('signup.ejs', { message: req.flash('signupMessage') });
 });
 
 router.get('/profile', isLoggedIn, function(req, res) {
@@ -33,6 +33,27 @@ router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/profile',
   failureRedirect: '/login',
   failureFlash: true,
+}));
+
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
+}));
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/profile',
+  failureRedirect: '/',
 }));
 
 module.exports = router;
